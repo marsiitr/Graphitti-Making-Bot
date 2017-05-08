b561bd360ba80b7af7007fc5daac685b022d5e69
DhruvGamdha <dhruvgamdha@gmail.com>
2017-05-08 10:05:27 +0530

    Add files via upload

---
+++ The Graffiti bot/motors.ino
@@ -0,0 +1,44 @@
+void motor_setup()
+{
+  pinMode(m1_dir1, OUTPUT);
+  pinMode(m1_dir2, OUTPUT);
+  pinMode(m1_pwm, OUTPUT);
+  pinMode(m2_dir1, OUTPUT);
+  pinMode(m2_dir2, OUTPUT);
+  pinMode(m2_pwm, OUTPUT);
+}
+void mov_m1(int x)
+{ 
+  if (x > 0)
+  { digitalWrite(m1_dir1, LOW);
+    digitalWrite(m1_dir2, HIGH);
+    analogWrite(m1_pwm, x);
+  }
+  else if (x < 0)
+  { digitalWrite(m1_dir1, HIGH);
+    digitalWrite(m1_dir2, LOW);
+    analogWrite(m1_pwm, -x);
+  }
+  else
+  { digitalWrite(m1_dir1, HIGH);
+    digitalWrite(m1_dir2, HIGH);
+  }
+}
+void mov_m2(int x)
+{ 
+  if (x > 0)
+  { digitalWrite(m2_dir1, LOW);
+    digitalWrite(m2_dir2, HIGH);
+    analogWrite(m2_pwm, x);
+  }
+  else if (x < 0)
+  { digitalWrite(m2_dir1, HIGH);
+    digitalWrite(m2_dir2, LOW);
+    analogWrite(m2_pwm, -x);
+  }
+  else
+  { digitalWrite(m2_dir1, HIGH);
+    digitalWrite(m2_dir2, HIGH);
+  }
+}
+
---
+++ The Graffiti bot/ultra.ino
@@ -0,0 +1,33 @@
+void ultra_setup()
+{
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+
+}
+void read_ultra()
+{
+  digitalWrite(trig, LOW);
+  delay(2);
+  digitalWrite(trig, HIGH);
+  delay(10);
+  digitalWrite(trig, LOW);
+  t = pulseIn(echo, HIGH, 10000);
+  //moving average
+  values_storage[moving_avg_const] = t / 58.5; //conversion into cm
+  moving_avg_const++;
+  moving_avg_const %= 10;
+  for (int i = 0; i < 10; i++)
+  {
+    sum += values_storage[i];
+  }
+  val_ultra = sum / 10;
+  sum = 0;
+}
+void display_value_ultra()
+{
+  Serial.print("ultra_val = ");
+  Serial.println(val_ultra);
+}
+
+
+
